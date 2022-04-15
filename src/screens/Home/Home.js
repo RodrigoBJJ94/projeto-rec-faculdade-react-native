5f41b3810b68b62aef021d975d5dee0c8c63b3cc
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-04-15 16:13:51 -0300

    Version 1.03

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -103,25 +103,25 @@ export default function Home() {
         if (nameProduct !== "" || classProduct !== "" ||
             descriptionProduct !== "" || amountProduct !== "") {
             return (
-                <View>
-                    <Text>Lista dos Produtos</Text>
-                    <View>
-                        <Text>Nome do Produto: {nameProduct}</Text>
-                        <Text>Classe do Produto: {classProduct}</Text>
-                        <Text>Descrição do Produto: {descriptionProduct}</Text>
-                        <Text>Quantidade do Produto: {amountProduct}</Text>
+                <View style={Styles.viewList}>
+                    <Text style={Styles.titleList}>Lista dos Produtos</Text>
+                    <View style={Styles.viewProductsList}>
+                        <Text style={Styles.productsList}>Nome do Produto: {nameProduct}</Text>
+                        <Text style={Styles.productsList}>Classe do Produto: {classProduct}</Text>
+                        <Text style={Styles.productsList}>Descrição do Produto: {descriptionProduct}</Text>
+                        <Text style={Styles.productsList}>Quantidade do Produto: {amountProduct}</Text>
                     </View>
-                    <TouchableOpacity onPress={() => setScreenHome()}>
-                        <Text>Voltar para o Menu</Text>
+                    <TouchableOpacity style={Styles.buttonList} onPress={() => setScreenHome()}>
+                        <Text style={Styles.buttonTextList}>Voltar para o Menu</Text>
                     </TouchableOpacity>
                 </View>
             );
         } else {
             return (
-                <View>
-                    <Text>Nenhum produto salvo</Text>
-                    <TouchableOpacity onPress={() => setScreenHome()}>
-                        <Text>Voltar para o Menu</Text>
+                <View style={Styles.viewEmptyList}>
+                    <Text style={Styles.titleEmptyList}>Nenhum produto salvo</Text>
+                    <TouchableOpacity style={Styles.buttonEmptyList} onPress={() => setScreenHome()}>
+                        <Text style={Styles.buttonTextEmptyList}>Voltar para o Menu</Text>
                     </TouchableOpacity>
                 </View>
             );
