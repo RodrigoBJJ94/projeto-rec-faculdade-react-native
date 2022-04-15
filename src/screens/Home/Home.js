9386866b80a82c97c7868e2cda8a4f696e976368
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-04-15 05:03:25 -0300

    Version 1.01

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,32 +1,105 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, Alert } from "react-native";
+//import Insert from "../Insert/Insert";
+//import List from "../List/List";
 
-export default function Home({ navigation }) {
-    const [insert, setInsert] = useState(true);
-    //let insert = true;
-    //let list = true;
-    //let remove = true;
+export default function Home() {
+    const [screen, setScreen] = useState("Home");
+    const [nameProduct, setNameProduct] = useState("");
+    const [classProduct, setClassProduct] = useState("");
+    const [descriptionProduct, setDescriptionProduct] = useState("");
+    const [amountProduct, setAmountProduct] = useState("");
 
-    function getInsert() {
-        navigation.navigate("Insert");
-        setInsert(insert);
-    }
+    if (screen === "Home") {
+        return getScreenHome();
+    } else if (screen === "Insert") {
+        return getScreenInsert();
+    } else if (screen === "List") {
+        return getScreenList();
+    };
 
-    return (
-        <View>
-            <TouchableOpacity onPress={() => getInsert()}>
-                <Text>Incluir</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("Insert", {
-                list: list
-            })}>
-                <Text>Listar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("Insert", {
-                remove: remove
-            })}>
-                <Text>Excluir</Text>
-            </TouchableOpacity>
-        </View>
-    );
+    function setScreenInsert() {
+        setScreen("Insert");
+        return getScreenInsert();
+    };
+
+    function setScreenList() {
+        setScreen("List");
+        return getScreenList();
+    };
+
+    function setScreenDelete() {
+        if (nameProduct !== "" || classProduct !== "" ||
+            descriptionProduct !== "" || amountProduct !== "") {
+            setNameProduct("")
+            setClassProduct("");
+            setDescriptionProduct("");
+            setAmountProduct("");
+            Alert.alert("Todos os produtos foram excluídos!");
+        } else {
+            Alert.alert("Ainda não foi incluído nenhum produto para ser excluído!");
+        };
+    };
+
+    function setScreenHome() {
+        setScreen("Home");
+        return getScreenHome();
+    };
+
+    function getScreenHome() {
+        return (
+            <View>
+                <Text>Menu</Text>
+                <View>
+                    <TouchableOpacity onPress={() => setScreenInsert()}>
+                        <Text>Incluir</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setScreenList()}>
+                        <Text>Listar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setScreenDelete()}>
+                        <Text>Excluir</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
+
+    function getScreenInsert() {
+        return (
+            <View>
+                <Text>Inclusão de Produtos</Text>
+                <View>
+                    <Text>Nome do Produto</Text>
+                    <TextInput onChangeText={text => setNameProduct(text)} />
+                    <Text>Classe do Produto</Text>
+                    <TextInput onChangeText={text => setClassProduct(text)} />
+                    <Text>Descrição do Produto</Text>
+                    <TextInput onChangeText={text => setDescriptionProduct(text)} />
+                    <Text>Quantidade do Produto</Text>
+                    <TextInput onChangeText={text => setAmountProduct(text)} />
+                </View>
+                <TouchableOpacity onPress={() => setScreenHome()}>
+                    <Text>Voltar para o Menu</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    };
+
+    function getScreenList() {
+        return (
+            <View>
+                <Text>Lista dos Produtos</Text>
+                <View>
+                    <Text>Nome do Produto: {nameProduct}</Text>
+                    <Text>Classe do Produto: {classProduct}</Text>
+                    <Text>Descrição do Produto: {descriptionProduct}</Text>
+                    <Text>Quantidade do Produto: {amountProduct}</Text>
+                </View>
+                <TouchableOpacity onPress={() => setScreenHome()}>
+                    <Text>Voltar para o Menu</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    };
 };
--- src/screens/List/List.js
+++ src/screens/List/List.js
@@ -1,19 +1,10 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
-export default function List({ nameProduct }) {
+export default function List() {
     return (
         <View>
-            <Text>Inclua um Produto</Text>
-            <View>
-                <Text>Nome do Produto: {nameProduct}</Text>
-                <Text>Classe do Produto</Text>
-                <Text>Descrição do Produto</Text>
-                <Text>Quantidade do Produto</Text>
-            </View>
-            <TouchableOpacity>
-                <Text>Excluir</Text>
-            </TouchableOpacity>
+         
         </View>
     );
 };
